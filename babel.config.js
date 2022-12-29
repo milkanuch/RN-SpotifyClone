1f4b8f066ea31db70729c77ee1749e183f5d05a7
misha.ilkanych <misha.ilkanych@ekreative.com>
2022-12-30 01:45:24 +0200

    build: add alias paths

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,18 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        alias: {
+          //SOURCE: https://github.com/tleunen/babel-plugin-module-resolver
+          components: './src/components',
+          constants: './src/constants',
+          screens: './src/screens',
+          navigation: './src/navigation',
+          assets: './src/assets',
+        },
+      },
+    ],
+  ],
 };
