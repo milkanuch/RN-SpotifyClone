83bb0f034abfac1624ada1c4601e779620ed6fe1
misha.ilkanych <misha.ilkanych@ekreative.com>
2022-12-30 01:14:29 +0200

    feat: add icon component

---
+++ src/components/Icon/icon.styles.ts
@@ -0,0 +1,11 @@
+import { StyleSheet } from 'react-native';
+
+import { COLORS } from './../../constants/colors';
+
+const styles = StyleSheet.create({
+  icon: {
+    tintColor: COLORS.white,
+  },
+});
+
+export default styles;
