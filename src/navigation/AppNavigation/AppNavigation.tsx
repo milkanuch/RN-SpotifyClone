7400c0687fa3a9decb11a23bb0944f44398b2886
misha.ilkanych <misha.ilkanych@ekreative.com>
2022-12-30 01:15:42 +0200

    feat: add app navigation

---
+++ src/navigation/AppNavigation/AppNavigation.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+
+import BottomTabNavigation from '../BottomTabNavigation/BottomTabNavigation';
+
+import { appTheme } from './appNavigation.settings';
+
+const AppNavigation = () => {
+  return (
+    <SafeAreaProvider>
+      <NavigationContainer theme={appTheme}>
+        <BottomTabNavigation />
+      </NavigationContainer>
+    </SafeAreaProvider>
+  );
+};
+
+export default AppNavigation;
