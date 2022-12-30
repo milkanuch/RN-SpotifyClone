cd40719f93a770d006b4565ee882e37aa01ac7a3
misha.ilkanych <misha.ilkanych@ekreative.com>
2022-12-30 22:46:01 +0200

    build: add react native config

---
+++ react-native.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  project: {
+    ios: {},
+    android: {},
+  },
+  //SOURCE: https://blog.logrocket.com/adding-custom-fonts-react-native/
+  assets: ['./src/assets/fonts/'],
+};
