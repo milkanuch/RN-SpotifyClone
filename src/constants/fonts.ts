276f0418ab0c94fb0ec37b4c79db5d8359fc8ca2
misha.ilkanych <misha.ilkanych@ekreative.com>
2023-01-03 21:26:43 +0200

    refactor: used a hash map instead const

--- src/constants/fonts.ts
+++ src/constants/fonts.ts
@@ -1,11 +1,8 @@
-export const OPEN_SANS_LIGHT = 'OpenSans-Light';
-
-export const OPEN_SANS_REGULAR = 'OpenSans-Regular';
-
-export const OPEN_SANS_MEDIUM = 'OpenSans-Medium';
-
-export const OPEN_SANS_BOLD = 'OpenSans-Bold';
-
-export const OPEN_SANS_SEMI_BOLD = 'OpenSans-SemiBold';
-
-export const OPEN_SANS_EXTRA_BOLD = 'OpenSans-ExtraBold';
+export const OPEN_SANS = {
+  light: 'OpenSans-Light',
+  regular: 'OpenSans-Regular',
+  medium: 'OpenSans-Medium',
+  bold: 'OpenSans-Bold',
+  semiBold: 'OpenSans-SemiBold',
+  extraBold: 'OpenSans-ExtraBold',
+};
