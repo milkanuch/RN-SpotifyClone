29fdafaf4b227b93f8c6f5b383786795b8cfb0db
misha.ilkanych <misha.ilkanych@ekreative.com>
2023-01-03 22:43:20 +0200

    feat: add greetingMessage const

--- src/screens/HomeScreen/HomeScreenTitle/HomeScreenTitle.tsx
+++ src/screens/HomeScreen/HomeScreenTitle/HomeScreenTitle.tsx
@@ -6,18 +6,20 @@ import Title from 'components/Title/Title';
 
 import { iconImages } from 'constants/icons';
 
-import { getGreetMessageByTime } from './homeScreenTitle.utils';
+import { getGreetingMessageByTime } from './homeScreenTitle.utils';
 
 import styles from './homeScreenTitle.styles';
 
 const HomeScreenTitle = () => {
+  const greetingMessage = getGreetingMessageByTime();
+
   const handleSettingsPress = () => {
     //TODO: navigation on settings screen
   };
 
   return (
     <View style={styles.container}>
-      <Title text={getGreetMessageByTime()} />
+      <Title text={greetingMessage} />
       <IconButton iconName={iconImages.Gears} onPress={handleSettingsPress} />
     </View>
   );
