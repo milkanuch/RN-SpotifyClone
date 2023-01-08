7ec953c7d55d6863acf6d3a2fab576701dd3adcd
misha.ilkanych <misha.ilkanych@ekreative.com>
2023-01-09 00:01:50 +0200

    feat: add album image

---
+++ src/screens/AlbumScreen/AlbumImage/AlbumImage.tsx
@@ -0,0 +1,58 @@
+import React, { FC } from 'react';
+import { Image } from 'react-native';
+
+import Animated, {
+  Extrapolation,
+  interpolate,
+  useAnimatedStyle,
+} from 'react-native-reanimated';
+
+import {
+  OPACITY_INPUT_RANGE,
+  OPACITY_OUTPUT_RANGE,
+  SCALE_INPUT_RANGE,
+  SCALE_OUTPUT_RANGE,
+} from './albumImage.settings';
+import styles from './albumImage.styles';
+import { AlbumImageProps } from './albumImage.types';
+
+const AlbumImage: FC<AlbumImageProps> = ({ imageUri, scrollY }) => {
+  const imageSource = {
+    uri: imageUri,
+  };
+
+  const animatedStyles = useAnimatedStyle(() => {
+    const scale = interpolate(
+      scrollY.value,
+      SCALE_INPUT_RANGE,
+      SCALE_OUTPUT_RANGE,
+      {
+        extrapolateRight: Extrapolation.CLAMP,
+        extrapolateLeft: Extrapolation.IDENTITY,
+      },
+    );
+
+    const opacity = interpolate(
+      scrollY.value,
+      OPACITY_INPUT_RANGE,
+      OPACITY_OUTPUT_RANGE,
+      {
+        extrapolateRight: Extrapolation.CLAMP,
+        extrapolateLeft: Extrapolation.IDENTITY,
+      },
+    );
+
+    return {
+      transform: [{ scale: scale }],
+      opacity: opacity,
+    };
+  });
+
+  return (
+    <Animated.View style={[styles.container, animatedStyles]}>
+      <Image source={imageSource} style={styles.image} />
+    </Animated.View>
+  );
+};
+
+export default AlbumImage;
---
+++ src/screens/AlbumScreen/AlbumImage/albumImage.settings.ts
@@ -0,0 +1,7 @@
+export const SCALE_INPUT_RANGE = [0, 500];
+
+export const SCALE_OUTPUT_RANGE = [1, 0];
+
+export const OPACITY_INPUT_RANGE = [0, 250];
+
+export const OPACITY_OUTPUT_RANGE = [1, 0];
