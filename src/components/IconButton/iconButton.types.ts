33516cb578b770ae80bc27f82e8259d27685b249
misha.ilkanych <misha.ilkanych@ekreative.com>
2023-01-08 23:46:43 +0200

    feat: add additional style props

--- src/components/IconButton/iconButton.types.ts
+++ src/components/IconButton/iconButton.types.ts
@@ -1,6 +1,13 @@
-import { ImageSourcePropType, TouchableOpacityProps } from 'react-native';
+import {
+  ImageSourcePropType,
+  ImageStyle,
+  TouchableOpacityProps,
+  ViewStyle,
+} from 'react-native';
 
 export interface IconButtonProps {
   iconName: ImageSourcePropType;
   onPress: TouchableOpacityProps['onPress'];
+  containerStyle?: ViewStyle;
+  iconStyle?: ImageStyle;
 }
