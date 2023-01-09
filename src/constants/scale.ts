6fd84547286170eb0b790cf6ff1c446d12b2241b
misha.ilkanych <misha.ilkanych@ekreative.com>
2023-01-10 01:44:35 +0200

    fix: status bar height

--- src/constants/scale.ts
+++ src/constants/scale.ts
@@ -1,10 +1,12 @@
-import { Dimensions, StatusBar } from 'react-native';
+import { Dimensions } from 'react-native';
+
+import Constants from 'expo-constants';
 
 export const { height, width } = Dimensions.get('window');
 
 export const SCREEN_HEIGHT = Dimensions.get('screen').height;
 
-export const STATUS_BAR_HEIGHT = StatusBar.currentHeight ?? 0;
+export const STATUS_BAR_HEIGHT = Constants.statusBarHeight ?? 0;
 
 export const SCREEN_PADDING = 15;
 
