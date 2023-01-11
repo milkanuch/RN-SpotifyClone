6222568b5d972f64feb3eed1564dd50061cb28ba
misha.ilkanych <misha.ilkanych@ekreative.com>
2023-01-11 13:36:53 +0200

    feat: add load app

--- App.tsx
+++ App.tsx
@@ -1,13 +1,24 @@
-import React, { useEffect } from 'react';
+import React, { useCallback, useEffect } from 'react';
 
 import SplashScreen from 'react-native-splash-screen';
 
 import AppNavigation from './src/navigation/AppNavigation/AppNavigation';
+import { getAlbumDetails } from './src/services/index';
+import { setAlbumDetails } from './src/store/albumDetailsSlice/albumDetails';
+import { useAppDispatch } from './src/store/index';
 
 const App = () => {
-  useEffect(() => {
+  const dispatch = useAppDispatch();
+
+  const loadApp = useCallback(async () => {
+    const data = await getAlbumDetails();
+    dispatch(setAlbumDetails(data));
     SplashScreen.hide();
-  }, []);
+  }, [dispatch]);
+
+  useEffect(() => {
+    loadApp();
+  }, [loadApp]);
 
   return <AppNavigation />;
 };
