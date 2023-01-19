2a28540da9feb21a4d962409dd0676a5a6d6e930
misha.ilkanych <misha.ilkanych@ekreative.com>
2023-01-19 13:36:34 +0200

    feat: configure redux-persist

--- index.js
+++ index.js
@@ -7,18 +7,21 @@ import { AppRegistry } from 'react-native';
 import ErrorBoundary from 'react-native-error-boundary';
 import TrackPlayer from 'react-native-track-player';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
 import App from './App';
 import { name as appName } from './app.json';
 import FallBack from './src/components/FallBack/FallBack';
 import { PlaybackService } from './src/services/player/PlayBackService';
-import { store } from './src/store/index';
+import { store, persistor } from './src/store/index';
 
 const Root = () => {
   return (
     <ErrorBoundary FallbackComponent={FallBack}>
       <Provider store={store}>
-        <App />
+        <PersistGate persistor={persistor}>
+          <App />
+        </PersistGate>
       </Provider>
     </ErrorBoundary>
   );
