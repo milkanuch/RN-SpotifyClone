c0054a8ddd1460ede30da5a2c23d0722693efda7
misha.ilkanych <misha.ilkanych@ekreative.com>
2023-01-20 17:30:54 +0200

    feat: add additional props

--- src/screens/PlayerScreen/SongTitle/songTitle.types.ts
+++ src/screens/PlayerScreen/SongTitle/songTitle.types.ts
@@ -1,4 +1,8 @@
+import { TouchableOpacityProps } from 'react-native';
+
 export interface SongTitleProps {
   songName?: string;
   songArtist?: string;
+  songId?: number;
+  onPress: TouchableOpacityProps['onPress'];
 }
